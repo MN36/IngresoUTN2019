583a6025dd11efdaaffcc755ef2ef07f22343df0
MN36 <47321730+MN36@users.noreply.github.com>
2019-02-18 12:26:29 -0300

    clase while
    clase while

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
-	alert('iteración while');
+	//alert('iteración while');
 
-
+	var cantidad;
+	var i=1
+	
+	while(i<11){
+		console.log(i);
+		i=i+1
+	}
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (2).js
+++ 5-InstruccionWhile/jsIteraciones (2).js
@@ -1,6 +1,16 @@
 function Mostrar()
 {
-	alert('iteración while');
+	//alert('iteración while');
+
+	var cantidad=10;
+	var i=-5;
+
+	while(i<10){
+		console.log(i);
+
+		i=i+1;
+
+	}
 
 
 }//FIN DE LA FUNCIÓN
