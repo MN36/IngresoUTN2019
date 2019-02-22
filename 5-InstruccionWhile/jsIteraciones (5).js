82b5e7908798484fe2af2d3cc9379cce61710933
MN36 <47321730+MN36@users.noreply.github.com>
2019-02-22 12:29:01 -0300

    clases while
    clase whle

--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -3,8 +3,14 @@ function Mostrar()
 
 var sexo = prompt("ingrese f ó m .");
 
+while(!(sexo=="m"|| sexo=="f"))
+{
+    alert("Incorrecto");
+    sexo=(prompt("Intente de nuevo"));
+    document.getElementById("Sexo").value=sexo;
+}
 
-
-document.getElementById('Sexo').value=sexo;
+alert("Correcto");
+document.getElementById("Sexo").value=sexo;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,21 @@
 function Mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+var contador=0;
+var acumulador=0;
+var numero=
+var promedio;
+var suma;
+
+while(numero <5){
+
+	suma=contador+acumulador;
+
+}
+  promedio=suma/5;
+  document.getElementById('suma').value=acumulador;
+  document.getElementById('promedio').value=acumulador/5;
 
 
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
 
 }//FIN DE LA FUNCIÓN
