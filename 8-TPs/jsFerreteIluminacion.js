da45089aa2ef2fbca3bfca4f785e8c4b698f62b1
MN36 <47321730+MN36@users.noreply.github.com>
2019-02-23 23:06:33 -0300

    tp fallido iluminacion con if
    tp fallido iluminacion con if

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,43 +10,112 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
+     var precio=35;
      var cantidad;
      var marca;
+     var descuento;
      var preciodescuento;
-     cantidad=parseFloat(document.getElementById("Cantidad").value);
-     marca=parseFloat(document.getElementById("Marca").value);
-     preciodescuento=parseFloat(document.getElementById("precioDescuento").value);
+     var importe;
+     var iibb;
 
-     if(cantidad>=6)
-     {cantidad=cantidad*35;
-     preciodescuento= cantidad-(cantidad*50/100);
 
-     document.getElementById("precioDescuento").value=preciodescuento;}
+     cantidad=parseInt(document.getElementById("Cantidad").value);
+     
+     if(cantidad>=6){
+          descuento=cantidad*50/100;
+     }
+     else if(cantidad==5){
+          if(marca=="ArgentinaLuz"){
+               descuento=cantidad*40/100;
+          }
+          else{ descuento=cantidad*30/100}
+     }
+     else if(cantidad==4){
+          if(marca=="ArgentinaLuz"|| marca=="FelipeLamparas" ){
+               descuento=cantidad*25/100;
+          }
+          else{descuento=cantidad*20/100;}
+     }
+     else if(cantidad==3){
+          if(marca=="ArgentinaLuz"){
+               descuento=cantidad*15/100;
+          }
+          else if(marca=="FelipeLamparas"){
+               descuento=cantidad*10/100;}
+               else{descuento=cantidad*5;}
+
+
+          }
+     else if(cantidad>0){
+          desuento=0;}
+          else{alert("Cantidad sin descuento");}     
+
+     }
+     if(importe>=120){
+          iibb=importe*10/100;
+          importe=iibb+importe;
+          alert("Usted pagó: "+ iibb+" de iibb,siendo: "+importe+" el Total que usted pagó");}
+
+          preciodescuento=precio-descuento;
+          importe=preciodescuento*cantidad;
+         document.getElementById("precioDescuento").value=preciodescuento;
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     //{cantidad=cantidad*precio;
+     //preciodescuento= cantidad-(cantidad*50/100);
+
+     //document.getElementById("precioDescuento").value=preciodescuento;}
 
      //HASTA ACA TODO BIEN//
-     else if(cantidad==5 && marca== "Argentinaluz")
-     {preciodescuento=cantidad-(cantidad*40/100);
-    document.getElementById("precioDescuento").value=preciodescuento;
-}
-     else
-    {preciodescuento=cantidad-(cantidad*30/100);
-    document.getElementById("precioDescuento").value=preciodescuento;
-}
-     if(cantidad==4 && marca== "Argentinaluz"|| "Felipelamparas")
-          {preciodescuento=cantidad-(cantidad*25/100);
-          document.getElementById("precioDescuento").value=preciodescuento;}
+     //else if(cantidad==5 && marca== "Argentinaluz")
+     //{preciodescuento=cantidad-(cantidad*40/100);
+    //document.getElementById("precioDescuento").value=preciodescuento;
+    //}
+     //else
+    //{preciodescuento=cantidad-(cantidad*30/100);
+    //document.getElementById("precioDescuento").value=preciodescuento;
+    //}
+    // if(cantidad==4 && marca== "Argentinaluz"|| "Felipelamparas")
+          ///{preciodescuento=cantidad-(cantidad*25/100);
+          //document.getElementById("precioDescuento").value=preciodescuento;}
 
-          else{
-               preciodescuento=cantidad-(cantidad*20/100);
-          }
-     if(cantidad ==3 && marca =="Argentinaluz")
-     { preciodescuento=cantidad-(cantidad*15/100);}
-     else if(cantidad==3 && marca=="Felipelamparas")
-     {preciodescuento=cantidad-(cantidad*10/100);}
-     else{preciodescuento=cantidad-(cantidad*5/100);}
+          //else{
+          //     preciodescuento=cantidad-(cantidad*20/100);
+        //  }
+     ///if(cantidad ==3 && marca =="Argentinaluz")
+     //{ preciodescuento=cantidad-(cantidad*15/100);}
+     //else if(cantidad==3 && marca=="Felipelamparas")
+     //{preciodescuento=cantidad-(cantidad*10/100);}
+     //else{preciodescuento=cantidad-(cantidad*5/100);}
      
-          if(preciodescuento>=120)
-     { iibb=(preciodescuento*10/100)+preciodescuento
-     alert("Usted pago "+ preciodescuento.toFixed(2)+ " siendo "+ iibb.toFixed(2)+ " el impuesto que se pago");}
-}
+       //   if(preciodescuento>=120)
+     //{ iibb=(preciodescuento*10/100)+preciodescuento
+     //alert("Usted pago "+ preciodescuento.toFixed(2)+ " siendo "+ iibb.toFixed(2)+ " el impuesto que se pago");}
 
+     //}
